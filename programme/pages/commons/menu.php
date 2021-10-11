27503f8e7867593e45801d0e2069a9e3c2f6eb83
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-11 21:25:42 +0200

    Partie 2 : Le menu

---
+++ programme/pages/commons/menu.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-dark perso_bgBlue">
+    <a class="navbar-brand" href="#">@H2PROG - Matthieu</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Accueil </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Présentation </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Mon CV </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Compétences </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Portfolio </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Témoignage </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Contact </a>
+            </li>
+        </ul>
+    </div>
+</nav>
